cab37c812a071a7d35c456deb79f2f000f6d9924
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 00:43:29 +0100

    authResponse dto done

--- src/main/java/org/example/dto/AuthResponse.java
+++ src/main/java/org/example/dto/AuthResponse.java
@@ -1,4 +1,67 @@
 package org.example.dto;
 
+import java.util.List;
+
 public class AuthResponse {
+
+    private String token;
+    private String tokenType = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String token, Long id, String username, String email, List<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // getters & setters
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
+
+
