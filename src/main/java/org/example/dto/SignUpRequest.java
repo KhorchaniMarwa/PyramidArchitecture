f4a0f150d1bd4c4051a860453a8a99c291602803
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 00:36:35 +0100

    sign up dto done

--- src/main/java/org/example/dto/SignUpRequest.java
+++ src/main/java/org/example/dto/SignUpRequest.java
@@ -19,4 +19,39 @@ public class SignUpRequest {
 
     @ValidPassword
     private String password;
+    public SignUpRequest() {
+    }
+
+    public SignUpRequest(String username, String email, String password, Set<String> roles) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // getters & setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
