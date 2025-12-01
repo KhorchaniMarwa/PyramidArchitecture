7eee0e5e11fb67f0f7a500f5ed5a7c0a14256810
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 00:34:45 +0100

    using of the annotation

--- src/main/java/org/example/dto/SignUpRequest.java
+++ src/main/java/org/example/dto/SignUpRequest.java
@@ -1,4 +1,22 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.example.validator.ValidPassword;
+
+import java.util.Set;
+
 public class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @ValidPassword
+    private String password;
 }
