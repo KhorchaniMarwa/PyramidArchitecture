222a94a36afc77408e7c52c76a52ddf727bda3be
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 20:25:32 +0100

    Repository code done

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+import org.example.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // For sign-in: find user by email
+    Optional<User> findByEmail(String email);
+
+    // For sign-up: check if email already exists
+    boolean existsByEmail(String email);
+
 }
