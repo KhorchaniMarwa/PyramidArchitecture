d2874523104eb1e87f92de63fe1a33a33f711ad2
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 20:03:09 +0100

    User entity done

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,4 +1,32 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.example.validator.ValidPassword;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @ValidPassword
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
 }
