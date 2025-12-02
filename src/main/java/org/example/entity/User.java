d83edf6adee2258250ea776431d6cb11c7d5d81c
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 20:24:50 +0100

    entity user done

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -19,12 +19,28 @@ public class User {
     @GeneratedValue
     private Long id;
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Column(nullable = false, unique = true)
     private String email;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 
     @Column(nullable = false)
     @ValidPassword
     private String password;
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
     @Column(nullable = false)
     private String fullName;
