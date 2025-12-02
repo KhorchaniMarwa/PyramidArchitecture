99fc857993beb49cbd73da9cdfb2d5e42f0b72aa
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-02 21:18:22 +0100

    business object code initialized

--- src/main/java/org/example/domain/UserBO.java
+++ src/main/java/org/example/domain/UserBO.java
@@ -1,4 +1,17 @@
 package org.example.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserBO {
+
+    private Long id;
+    private String email;
+    private String fullName;
 }
