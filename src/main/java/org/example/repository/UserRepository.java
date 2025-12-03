5017e6ac163a2207cdcc3104a6b67a0f7372da6f
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-03 22:20:02 +0100

    comments updated

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // For sign-in: find user by email
+    //For sign-in: find user by email
     Optional<User> findByEmail(String email);
 
     // For sign-up: check if email already exists
