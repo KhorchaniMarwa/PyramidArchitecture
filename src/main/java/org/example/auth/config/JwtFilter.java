84037641b57283c54f4e6cc8290a6ce79d887232
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-12-05 00:11:24 +0100

    adding constructor

--- src/main/java/org/example/auth/config/JwtFilter.java
+++ src/main/java/org/example/auth/config/JwtFilter.java
@@ -15,4 +15,14 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter{
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtFilter(JwtService jwtService, UserDetailsService userDetailsService){
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletResponse response, HttpServletRequest request, FilterChain filterChain) throws ServletException, IOException{
+        String authHeader = request.getHeader("Authorization");
+    }
 }
